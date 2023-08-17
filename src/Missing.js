dc853ba3178213fc1b8351e061230e82e7ad9cdb
Pd Rajan <itspdrajan@gmail.com>
2023-08-18 01:28:49 +0530

    added components

--- src/Missing.js
+++ src/Missing.js
@@ -1,8 +1,17 @@
-
+import { Link } from "react-router-dom"
 
 const Missing = () => {
   return (
-    <main><h1>Missing</h1></main>
+    <main className="Missing">
+
+      <h2>Page not Found</h2>
+      <p>Well, that's Disappointing</p>
+      <p>
+        <Link to='/'>Visit Our Homepage</Link>
+      </p>
+
+
+    </main>
   )
 }
 
